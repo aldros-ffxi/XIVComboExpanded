13c48db1901fae88bd21e6e44dd7075dd64be923
Khayle <MKhayle@users.noreply.github.com>
2024-08-07 00:45:04 +0200

    MCH update
    Excavator, my beloved

--- XIVComboExpanded/Combos/MCH.cs
+++ XIVComboExpanded/Combos/MCH.cs
@@ -274,8 +274,11 @@ internal class MachinistAirAnchorChainsaw : CustomCombo
 
     protected override uint Invoke(uint actionID, uint lastComboMove, float comboTime, byte level)
     {
-        if (actionID == MCH.HotShot || actionID == MCH.AirAnchor || actionID == MCH.Chainsaw)
+        if (actionID == MCH.HotShot || actionID == MCH.AirAnchor || actionID == MCH.Chainsaw || actionID == MCH.Excavator)
         {
+            if (level >= MCH.Levels.Excavator && HasEffect(MCH.Buffs.ExcavatorReady))
+                return MCH.Excavator;
+
             if (level >= MCH.Levels.Chainsaw)
                 return CalcBestAction(actionID, MCH.Chainsaw, MCH.AirAnchor);
 
