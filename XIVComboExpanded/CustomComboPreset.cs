472266b5e0e969b63b2c59931d165360ade219c4
Kaedys <kaedyskor@gmail.com>
2024-08-06 00:45:50 -0400

    [BUGFIX] Dragoon Combos
    - Fixed bug in Dragoon Vorpal Thrust Option and Disembowel Option that prevented them from working
    - Fixed bug causing both of the main Dragoon combo options to affect both buttons.
    - Updated main single-target combo block structure.
    - Pulled in Dragoon All-In-One combo from my local files, and included a header pending the Attributes Rework
    - Pulled in a single target version of the AoE Wyrmwind Feature, also with a Rework header.
    - Reworded the descriptions of the two Double combo features to clarify what they do.  Also swapped the var names to match the combo names.
    - Renamed BarrageThrust to LanceBarrage and ExplosiveThrust to SpiralBlow, to match English client naming.
    - Removed duplicate BarrageThrust2 and ExplosiveThrust2 definitions.
    - Added buff and debuff definitions the new All-In-One logic depends on.

--- XIVComboExpanded/CustomComboPreset.cs
+++ XIVComboExpanded/CustomComboPreset.cs
@@ -1,3 +1,4 @@
+using Dalamud.Utility;
 using XIVComboExpandedPlugin.Attributes;
 using XIVComboExpandedPlugin.Combos;
 
@@ -426,6 +427,14 @@ public enum CustomComboPreset
     // ====================================================================================
     #region DRAGOON
 
+    // [IconsCombo([DRG.RaidenThrust, DRG.LanceBarrage, DRG.SpiralBlow, DRG.HeavensThrust, DRG.ChaoticSpring, DRG.FangAndClaw, DRG.WheelingThrust, DRG.Drakesbane, UTL.Cycle])]
+    // [SectionCombo("Single Target")]
+    // [AccessibilityCustomCombo]
+    [ConflictingCombos(DragoonFullThrustCombo, DragoonChaosThrustCombo)]
+    [CustomComboInfo("All-In-One Combo", "Replace Full Thrust and Chaos Thrust with the entire 10-part combo chain, auto-selecting the Chaos Thrust combo as needed to refresh Power Surge or the bleed\n\nNOTE: This can cause you to miss positionals.", DRG.JobID)]
+    DragoonAllInOneCombo = 2216,
+
+    [ConflictingCombos(DragoonAllInOneCombo)]
     [CustomComboInfo("Full Thrust Combo", "Replace Full Thrust with its combo chain.", DRG.JobID)]
     DragoonFullThrustCombo = 2204,
 
@@ -434,9 +443,10 @@ public enum CustomComboPreset
     DragoonFullThrustComboOption = 2210,
 
     [ParentCombo(DragoonFullThrustCombo)]
-    [CustomComboInfo("Double Chaos Thrust Option", "Replicates the Full Thrust combo while not in the Chaotic Thrust combo.", DRG.JobID)]
-    DragoonDoubleFullThrustComboOption = 2215,
+    [CustomComboInfo("Double Chaos Thrust Option", "After using Disembowel, replicates the remainder of the Chaos Thrust combo on Full Thrust, starting at Chaos Thrust.  Combined with the Double Full Thrust Option, this allows you to select which combo to use at the 2nd combo step, but the remainder of both combos will be on both buttons", DRG.JobID)]
+    DragoonDoubleChaosThrustComboOption = 2215,
 
+    [ConflictingCombos(DragoonAllInOneCombo)]
     [CustomComboInfo("Chaos Thrust Combo", "Replace Chaos Thrust with its combo chain.", DRG.JobID)]
     DragoonChaosThrustCombo = 2203,
 
@@ -445,8 +455,14 @@ public enum CustomComboPreset
     DragoonChaosThrustComboOption = 2209,
 
     [ParentCombo(DragoonChaosThrustCombo)]
-    [CustomComboInfo("Double Full Thrust Option", "Replicates the Chaotic Thrust combo while not in the Full Thrust combo.", DRG.JobID)]
-    DragoonDoubleChaosComboOption = 2214,
+    [CustomComboInfo("Double Full Thrust Option", "After using Vorpal Thrust, replicates the remainder of the Full Thrust combo on Chaos Thrust, starting at Full Thrust.  Combined with the Double Choas Thrust Option, this allows you to select which combo to use at the 2nd combo step, but the remainder of both combos will be on both buttons", DRG.JobID)]
+    DragoonDoubleFullThrustComboOption = 2214,
+
+    // [IconsCombo([DRG.HeavensThrust, DRG.ChaoticSpring, UTL.ArrowLeft, DRG.WyrmwindThrust, UTL.Idea])]
+    // [SectionCombo("Single Target")]
+    // [AccessibilityCustomCombo]
+    [CustomComboInfo("Full Chaos Wyrmwind Feature", "Replace Full Thrust and Chaos Thrust with Wyrmwind Thrust when you have two Firstminds' Focus.", DRG.JobID)]
+    DragoonFullChaosWyrmwindFeature = 2217,
 
     [CustomComboInfo("Coerthan Torment Combo", "Replace Coerthan Torment with its combo chain.", DRG.JobID)]
     DragoonCoerthanTormentCombo = 2202,
